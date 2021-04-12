9e031f259aa72b10991c82df23f6019402005553
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2021-04-12 20:48:37 +0100

    Use event delegation within Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,18 +9,18 @@ function Game({ choices, setUser }) {
    */
   const handleClick = event => {
     const button = event.target.closest('button');
+    if (!button) return;
     setUser(button.dataset.id);
   };
 
   return (
-    <article className="game">
+    <article className="game" onClick={handleClick}>
       {choices.map(choice => (
         <button
           className={`game__button game__button--${choice}`}
           type="button"
           data-id={choice}
           key={choice}
-          onClick={handleClick}
         >
           <img src={icons[choice]} alt={choice} />
         </button>
