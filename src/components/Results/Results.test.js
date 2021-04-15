613d0d4bd7875a944d239da5f2b9c0d11de5adfe
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2021-04-15 11:33:52 +0100

    Add more test cases to Results component

--- src/components/Results/Results.test.js
+++ src/components/Results/Results.test.js
@@ -1,20 +1,62 @@
 import { render, screen } from '@testing-library/react';
 import Results from './Results';
 
-test('renders win', () => {
-  render(<Results user="rock" computer="scissors" score={0} />);
-  const heading = screen.getByRole('heading', { level: 2 });
-  expect(heading).toHaveTextContent(/you win/i);
+describe('wins', () => {
+  test('rock beats scissors', () => {
+    render(<Results user="rock" computer="scissors" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/you win/i);
+  });
+  
+  test('paper beats rock', () => {
+    render(<Results user="paper" computer="rock" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/you win/i);
+  });
+  
+  test('scissors beats paper', () => {
+    render(<Results user="scissors" computer="paper" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/you win/i);
+  });
 });
 
-test('renders loss', () => {
-  render(<Results user="scissors" computer="rock" score={0} />);
-  const heading = screen.getByRole('heading', { level: 2 });
-  expect(heading).toHaveTextContent(/you lose/i);
+describe('losses', () => {
+  test('scissors loses to rock', () => {
+    render(<Results user="scissors" computer="rock" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/you lose/i);
+  });
+
+  test('rock loses to paper', () => {
+    render(<Results user="rock" computer="paper" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/you lose/i);
+  });
+
+  test('paper loses to scissors', () => {
+    render(<Results user="paper" computer="scissors" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/you lose/i);
+  });
 });
 
-test('renders draw', () => {
-  render(<Results user="paper" computer="paper" score={0} />);
-  const heading = screen.getByRole('heading', { level: 2 });
-  expect(heading).toHaveTextContent(/draw/i);
+describe('draws', () => {
+  test('rock draws with rock', () => {
+    render(<Results user="rock" computer="rock" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/draw/i);
+  });
+
+  test('paper draws with paper', () => {
+    render(<Results user="paper" computer="paper" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/draw/i);
+  });
+
+  test('scissors draws with scissors', () => {
+    render(<Results user="scissors" computer="scissors" score={0} />);
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent(/draw/i);
+  });
 });
